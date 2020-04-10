91c1f703992f4ae48005de6adca212b306bc4a0d
Gianzin <unconfigured@null.spigotmc.org>
2020-04-09 22:11:41 -0300

    kk

--- Sapower/Assets/Script/Player/PlayerController.cs
+++ Sapower/Assets/Script/Player/PlayerController.cs
@@ -35,43 +35,23 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
     
-    //Animacoes
-    void Animacoes(){
-
+    //Animações
+    void Animações(){
         anim.SetInteger("state", (int)state);
         
-        if(rb.velocity.x==0){
-            
-            anim.SetBool("running", false);
-        }
-        else{
-            
-            anim.SetBool("running", true);
-        }
-        
-        //inicio do pulo
-        if(rb.velocity.y>0.1){
-
-            if(state==State.jumping){
-                anim.SetBool("Pulando", true);
-            }
-            else{
-                anim.SetBool("Pulando", false);
-                anim.SetBool("Pulo-noar", true);
-                
-            }
-
-
-        }
-
         //caindo
-        if(!noChao && rb.velocity.y<-0.1){
-            anim.SetBool("Pulo-noar", false);
-                
+        if(!noChao && rb.velocity.y<-0.5){
             state = State.falling;
         }
         else{
             state = State.idle;
+        }
+
+        //inicio do pulo
+        if(!noChao && rb.velocity.y>0.5){
+            state = State.jumping;
+            state = State.jump_air;
+
         }
 
     }
@@ -106,7 +86,6 @@ public class PlayerController : MonoBehaviour
         if(Input.GetButton("Jump") && contador<tempoDePulo){
             contador++;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-
         }
 
         TocarChao();
@@ -137,7 +116,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement();
-        Animacoes
-();
+        Animações();
     }
 }
