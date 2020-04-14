4177beaf6b6b0e8003438878a3e8840cfeb5cd89
Gianzin <unconfigured@null.spigotmc.org>
2020-04-13 22:32:30 -0300

    teste de cutscene e ajuste na colisão do chão

---
+++ Sapower/Assets/testes/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnEnable()
+    {
+        GameObject.Find("player_real").GetComponent<SpriteRenderer>().enabled = true;
+        GetComponent<Animator>().SetBool("cutscene", false);
+    }
+}
---
+++ Sapower/Assets/testes/cuscne.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cuscne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("player_cutscene").GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player"){
+            
+        GameObject.Find("player_cutscene").GetComponent<SpriteRenderer>().enabled = true;
+        other.GetComponent<SpriteRenderer>().enabled = false;
+        GameObject.Find("player_cutscene").GetComponent<Animator>().SetBool("cutscene", true);
+        Destroy(gameObject);
+
+        }
+    }
+
+}
